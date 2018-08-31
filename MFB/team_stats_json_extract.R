a3e874d1363e7e14da3c451c1747a58247554db3
Meyappan <meysubb@gmail.com>
2018-08-30 23:23:20 -0500

    add a couple of lines and thoughts

--- MFB/team_stats_json_extract.R
+++ MFB/team_stats_json_extract.R
@@ -14,4 +14,8 @@ team_info <- tamu_json$meta$teams
 team_stats <- tamu_json$teams[[2]]
 
 p <- team_stats[[1]]
+### pull breakdown out, and look through the dataframe and append all of these
+### neeed to do to this for both home and away team
+### what should the end result look like for team stats?
 l <- p$breakdown[[1]]
+l$stat <- paste0(p$stat[1],"-",l$stat)
