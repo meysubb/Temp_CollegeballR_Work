752b4716e9ed98067ea54180b12de5e5804f1b76
Meyappan <meysubb@gmail.com>
2018-09-08 17:10:47 -0500

    finish up the team_stats per game function

--- MFB/team_stats_json_extract.R
+++ MFB/team_stats_json_extract.R
@@ -1,21 +1,8 @@
 ### Scrape NCAA College Football
-### Team stats
-library(jsonlite)
+### Team stats by game
 
-pre_string <- "http://data.ncaa.com/sites/default/files/data/"
-post_string_options <- "teamStats.json"
-
-tamu_json <- fromJSON("http://data.ncaa.com/sites/default/files/data/game/football/fbs/2017/09/03/texas-am-ucla/teamStats.json")
-
-team_info <- tamu_json$meta$teams
-
-team_stats <- tamu_json$teams[[2]]
-
-
-### pull breakdown out, and look through the dataframe and append all of these
-### neeed to do to this for both home and away team
-### what should the end result look like for team stats?
-extract_team_stats <- function(df){
+### clean individual game team stats
+.cfb_clean_game_stats <- function(df){
   main_stat <- df
   add_lst <- df$breakdown
   names(add_lst) <- main_stat$stat
@@ -26,11 +13,8 @@ extract_team_stats <- function(df){
   stat_df <- rbind(main_stat,add_df)
 }
 
-### what do for season data?
-### this extracts data per game
-### Use .extract_base_schedule to get all season data
-### Loop over the season url's for this to work
-team_stats <- function(url){
+### extract team stats per game
+.cfb_team_stats_by_game <- function(url,dat){
   require(jsonlite)
   post_string_options <- "/teamStats.json"
   full_url <- paste0(url,post_string_options)
@@ -45,38 +29,66 @@ team_stats <- function(url){
   len <- length(l_teams)
   stat_df <- data.frame()
   for(i in 1:len){
-    df <- extract_team_stats(l_teams[[i]])
+    df <- .cfb_clean_game_stats(l_teams[[i]])
     df$name <- team_names[[i]]
     stat_df <- rbind(stat_df,df)
   }
+  stat_df$date <- dat
   return(stat_df)
 }
 
-#### Extracting Season's worth of urls
-team_id <- 697
-year <- 2018
-sport <- "MFB"
-df <- .extract_base_schedule(team_id,year,sport)
-year_id <- .year_ref(year,sport)
-base_url <- paste0("http://stats.ncaa.org/player/game_by_game?game_sport_year_ctl_id=",year_id,"&org_id=",team_id,"&stats_player_seq=-100")
-href <- read_html(base_url) %>%
-  html_nodes(".smtext .skipMask") %>%
-  html_attr("href")
-### remove hashtags
-clean_href <- gsub("\\#.*","",href)
-clean_href <- clean_href[clean_href != ""]
-actual_len = length(clean_href)
-if(actual_len == nrow(df)){
-  ### extract the following text
-  pre_string <- "http://data.ncaa.com/sites/default/files/data/game/football/fbs/"
-  extract_href <- gsub(".*fbs/","",clean_href)
-  df$href <- paste0(pre_string,extract_href)
+
+
+### final wrapper to get team stats
+.cfb_team_game <- function(team_id,year,sprt="MFB"){
+  ### check sport code first
+  sport <- sport_code(sprt)
+  ### Some pre-processing get base schedule and HREF Urls
+  df <- .extract_base_schedule(team_id,year,sport)
+  year_id <- .year_ref(year,sport)
+  base_url <- paste0("http://stats.ncaa.org/player/game_by_game?game_sport_year_ctl_id=",year_id,"&org_id=",team_id,"&stats_player_seq=-100")
+  href <- read_html(base_url) %>%
+    html_nodes(".smtext .skipMask") %>%
+    html_attr("href")
+  ### remove hashtags
+  clean_href <- gsub("\\#.*","",href)
+  clean_href <- clean_href[clean_href != ""]
+  actual_len = length(clean_href)
+  if(actual_len == nrow(df)){
+    ### extract the following text
+    pre_string <- "http://data.ncaa.com/sites/default/files/data/game/football/fbs/"
+    extract_href <- gsub(".*fbs/","",clean_href)
+    df$href <- paste0(pre_string,extract_href)
+  }
+
+  ## extract and store team names
+  teams <- team_mapping(year,sport)
+  t_name <- teams$team_name[which(team_id == teams$team_id)]
+
+
+  ### Extract data per game
+  ### use purrr safely to avoid potential URL errors
+  data_df <- df %>% mutate(
+    t_stats = purrr::map2(href,Date,purrr::safely(.cfb_team_stats_by_game)),
+    t_stats = sapply(t_stats,function(x){x[[1]]})
+  )
+
+  null_warning_ind <- unlist(lapply(data_df$t_stats,is.null))
+  data_errors <- paste0(t_name,"-",data_df[null_warning_ind,"Opponent"])
+  ### Need to print this out and return
+  keep_data <- data_df[!null_warning_ind,] %>% select(-href)
+  keep_data <- keep_data %>% mutate(team = t_name)
+  merge_dat <- keep_data %>% select()
+
+  game_stats <- keep_data %>% select(t_stats) %>% tidyr::unnest()
+  final_df <- tidyr::spread(data = game_stats,
+                     key = stat,
+                     value = data) %>% arrange(date)
+  if(length(data_errors)>0){
+    warnings(data_errors)
+  }
+  return(final_df)
 }
 
-### Store Team Name
-teams <- team_mapping(2018,sport)
-t_name <- teams$team_name[which(team_id == teams$team_id)]
-### Extract data per game
-data_df <- df %>% mutate(
-  t_stats = purrr::map(href,team_stats)
-)
+
+
