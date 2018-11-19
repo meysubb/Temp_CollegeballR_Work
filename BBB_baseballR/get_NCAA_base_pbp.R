dd34564f79dbdbc8c35275bfb052b54f9c472a96
Meyappan <meysubb@gmail.com>
2018-11-18 22:40:05 -0600

    clean up and integrate scraping pbp functions

---
+++ BBB_baseballR/get_NCAA_base_pbp.R
@@ -0,0 +1,108 @@
+library(tidyverse)
+library(XML)
+library(stringr)
+library(RCurl)
+
+stripwhite <- function(x) gsub("\\s*$", "", gsub("^\\s*", "", x))
+
+base_url='http://stats.ncaa.org/game/play_by_play/'
+
+
+score_fill=function(score_in){
+  m=length(score_in)
+  score_in[1]=0
+  for(i in 2:m){
+    if (is.na(score_in[i])){
+      score_in[i]=score_in[i-1]
+    }
+  }
+  return(score_in)
+}
+
+clean_games = function(game_id,year){
+  base_url='http://stats.ncaa.org/game/play_by_play/'
+  x= paste(base_url, game_id, sep='/')
+  x= paste(base_url, game_id, sep='/')
+  y=getURL(x)
+  # Play by play is in table form
+  y=readHTMLTable(y)
+  y = y[which(!sapply(y,is.null))]
+  
+  if (length(y) == 0 | length(y) < ncol(y[[3]])) {
+    print(paste("Play by Play data not available for game", game_id, sep=' '))
+    next
+  }
+  else{
+    j=1
+    for (i in 1:length(y)){
+      # Disgard NULL tables
+      if (is.null(y[[i]])==FALSE){
+        # Only take pbp tables (3 cols)
+        if (ncol(y[[i]])==3){
+          inn=as.data.frame(y[[i]])%>%
+            mutate(inning=j,
+                   game_id=game_id,
+                   year=year)%>%
+            select(year,game_id,inning,everything())
+          j=j+1
+          if(j==2){
+            pbp = inn
+          }else{
+            pbp = rbind(pbp,inn)
+          }
+        }
+      }
+    }
+  }
+  pbp = pbp %>% mutate(away_team = colnames(pbp)[4],
+             home_team = colnames(pbp)[6],
+             away_score = as.integer(gsub('-.*', '', Score)),
+             home_score = as.integer(gsub('.*-', '', Score)),
+             away_score=score_fill(away_score),
+             home_score=score_fill(home_score))%>%
+    rename(away_text = 4,
+           home_text = 6)%>%
+    filter(substr(away_text,1,3)!='R: ')%>%
+    select(year, game_id, inning, away_team, home_team, away_score, home_score, away_text, home_text)
+  return(pbp)
+}
+
+# Get pbp from all games for one team
+get_team_pbp=function(teamid, year, division=1){
+  all_team_games=get_team_schedule(teamid, year, division=1)
+  
+  
+  games=all_team_games%>%
+    distinct(GameId, .keep_all = TRUE)%>%
+    mutate(home_team=ifelse(Loc%in%c('H', 'N'), Team, Opp),
+           away_team=ifelse(Loc=='H', Opp, Team ))%>%
+    select(Year, Date, GameId, home_team, away_team)
+  
+    ### use purrr map here to
+  game_pbp = games %>% mutate(
+    pbp_raw = purrr::map2(GameId,Year,clean_games)
+  )  
+  pbp_final <- game_pbp %>% select(pbp_raw) %>% unnest()  
+  return(pbp_final)
+  }
+
+# Get pbp from all games in one season
+get_season_pbp=function(year, division=1,conference=NULL){
+  if(!is.null(conference)){
+    all_season_games=get_season_schedule(year, div=divison,conf=conference)
+  }
+  all_season_games=get_season_schedule(year, div=division)
+  
+  games=all_season_games %>%
+    distinct(GameId, .keep_all = TRUE) %>%
+    mutate(home_team=ifelse(Loc%in%c('H', 'N'), Team, Opp),
+           away_team=ifelse(Loc=='H', Opp, Team )) %>% 
+    select(Year, Date, GameId, home_team, away_team)
+ 
+  season_pbp = games %>% mutate(
+    pbp_raw = purrr::map2(GameId,Year,clean_games)
+  )
+  pbp_final <- season_pbp %>% select(pbp_raw) %>% unnest()  
+  return(pbp_final)
+}
+
--- BBB_baseballR/get_NCAA_base_schedule.R
+++ BBB_baseballR/get_NCAA_base_schedule.R
@@ -15,121 +15,97 @@ get_game_id=function(x){
 
 
 # Get one team's schedule in one year
-get_team_schedule <- function(teamid, year, division=1){
+get_team_schedule <- function(teamid, year, division=1,verbose=F){
   ### need a stop criteria here to make sure that the team id is correct
   assert_that(year>=2013,msg='you must provide a year that is equal to or greater than 2013')
   
-  
+  team <- master_ncaa_team_lu[(master_ncaa_team_lu$year == year) & 
+                                (master_ncaa_team_lu$school_id == teamid) &
+                                (master_ncaa_team_lu$division == division)
+                                ,"school"] %>% pull()
   base_url <- "http://stats.ncaa.org"
   yearid = ncaa_season_id_lu[ncaa_season_id_lu$season == year,"id"] %>% pull()
   team_url <- paste(base_url, "/team/index/", yearid, "?org_id=", teamid, sep="")
   
-  team_read <- team_url %>% read_html()
-  x <- team_read %>% html_nodes(".smtext") %>% html_text()
-  x_df <- apply(x %>% matrix(ncol=3,byrow=T),2,stripwhite) %>% data.frame(stringsAsFactors = F)
-  colnames(x_df) <- team_read %>% html_nodes("th") %>% html_text()
+  team_read <- try(team_url %>% read_html())
+  if (class(team_read)=='try-error'){
+    print(paste('Cannot connect to server for', team, 'in', year))
+    return(NULL)
+  }
+  else{
+    x <- team_read %>% html_nodes(".smtext") %>% html_text()
+    x_df <- apply(x %>% matrix(ncol=3,byrow=T),2,stripwhite) %>% data.frame(stringsAsFactors = F)
+    colnames(x_df) <- team_read %>% html_nodes("th") %>% html_text()
+    
+    game_ids <- get_game_id(team_read %>% html_nodes(".smtext .skipMask") %>% html_attr("href"))
+    
+    if(verbose){
+      print(paste0("Processing data for: ",team))
+    }
+    
+    x_df <- x_df %>% mutate(
+      game_id = game_ids,
+      team = team,
+      year = year,
+      loc = case_when(
+        grepl("^@", Opponent) ~ "A",
+        grepl(" @ ", Opponent, fixed = TRUE) ~ "N",
+        TRUE ~ "H"
+      ),
+      just_runs_result = gsub("\\([^()]*\\)", "", str_sub(Result,3,-1)),
+      team_runs = as.numeric(gsub("\\D", "", gsub("-.*$", "", just_runs_result))),
+      opp_runs = as.numeric(gsub(".*-", "", just_runs_result)),
+      win = case_when(team_runs<opp_runs ~ "L",
+                      team_runs>opp_runs ~ "W"),
+      innings = as.numeric(ifelse(
+        gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.", "", Result, perl = T) == "",
+        9,
+        gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.", "", Result, perl = T)
+      )),
+      opp = case_when(
+        loc == "A" ~ gsub("@", "", Opponent),
+        loc == "H" ~ Opponent,
+        loc == "N" ~ substring(Opponent, 1, regexpr("@", Opponent) - 2)
+      ),
+      total_w = cumsum(ifelse(win == "W",1,0)),
+      total_l = cumsum(ifelse(win == "L",1,0)),
+      record = paste0(total_w,"-",total_l)
+    )
+    
+    team_schedule <- x_df %>% select(year,Date,game_id,team,opp,loc,win,team_runs,opp_runs,record)
+    ## proper capitlization because I'm OCD.
+    colnames(team_schedule) <- to_upper_camel_case(colnames(team_schedule))
+    
+    return(team_schedule)
+  }
   
-  game_ids <- get_game_id(team_read %>% html_nodes(".smtext .skipMask") %>% html_attr("href"))
- 
-  team <- master_ncaa_team_lu[(master_ncaa_team_lu$year == year) & (master_ncaa_team_lu$school_id == teamid),"school"] %>% pull()
-  x_df <- x_df %>% mutate(
-    game_id = game_ids,
-    team = team,
-    year = year,
-    loc = case_when(
-      grepl("^@", Opponent) ~ "A",
-      grepl(" @ ", Opponent, fixed = TRUE) ~ "N",
-      TRUE ~ "H"
-    ),
-    just_runs_result = gsub("\\([^()]*\\)", "", str_sub(Result,3,-1)),
-    team_runs = as.numeric(gsub("\\D", "", gsub("-.*$", "", just_runs_result))),
-    opp_runs = as.numeric(gsub(".*-", "", just_runs_result)),
-    win = case_when(team_runs<opp_runs ~ "L",
-                    team_runs>opp_runs ~ "W"),
-    innings = as.numeric(ifelse(
-      gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.", "", Result, perl = T) == "",
-      9,
-      gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.", "", Result, perl = T)
-    )),
-    opp = case_when(
-      loc == "A" ~ gsub("@", "", Opponent),
-      loc == "H" ~ Opponent,
-      loc == "N" ~ substring(Opponent, 1, regexpr("@", Opponent) - 2)
-    ),
-    total_w = cumsum(ifelse(win == "W",1,0)),
-    total_l = cumsum(ifelse(win == "L",1,0)),
-    record = paste0(total_w,"-",total_l)
-  )
-  
-  team_schedule <- x_df %>% select(year,Date,game_id,team,opp,loc,win,team_runs,opp_runs,record)
-  ## proper capitlization because I'm OCD.
-  colnames(team_schedule) <- to_upper_camel_case(colnames(team_schedule))
-
-  return(team_schedule)
 }
 
 
 ############################################
-### come back and use purrr map 
-### to go through all teams for taht year
-### and scrape schedule. 
-### maybe don't display records? or figure that out
-
 # Get schedule for every team over years
-get_season_schedule = function(year_start, year_end, division=1){
-  if(year_start>year_end){
-    print('End year must same as, or later than, starting year')
+get_season_schedule = function(yr, div=1,conf=NULL){
+  
+  assert_that(year>=2013,msg='you must provide a year that is equal to or greater than 2013')
+  if(!is.null(conference)){
+    assert_that(conf %in% master_ncaa_team_lu$conference,
+                msg='Conference name provided is not clear, please check master_ncaa_team_lu DF')
+    teams_df <- master_ncaa_team_lu %>% filter(year==yr & division==div,conference==conf) %>% 
+      select(school,conference,school_id,year) %>% 
+      mutate(
+        schedule = purrr::map2(school_id,year,get_team_schedule,division=div,verbose=T)
+      ) %>% drop_na()
+    teams_clean <- teams_df %>% unnest() %>% select(-school,-year)
+  }else{
+    teams_df <- master_ncaa_team_lu %>% filter(year==yr & division==div) %>% 
+      select(school,conference,school_id,year) %>% 
+      mutate(
+        schedule = purrr::map2(school_id,year,get_team_schedule,division=div,verbose=T)
+      ) %>% drop_na()
+    teams_clean <- teams_df %>% unnest() %>% select(-school,-year)
   }
-  else{
-    year_ids = get_year_ids(year_start, year_end, division=1)
-    team_ids = get_team_ids(year_start, year_end, division=1)
-    
-    for (i in 1:nrow(team_ids)){
-      for (j in 1:nrow(year_ids)){
-        team <- toString(team_ids[i,'team'])
-        teamid <- toString(team_ids[i,'id'])
-        
-        year <- toString(year_ids[j,'year'])
-        yearid <- toString(year_ids[j,'year_id'])
-        
-        team_url <- paste(base_url, "/team/index/", yearid, "?org_id=", teamid, sep="")
-        
-        x <- try(scan(team_url, what="", sep="\n"))
-        if (class(x)=='try-error'){
-          print(paste('Cannot connect to server for', team, 'in', year))}
-        else{
-          if (length(grep(">\\d\\d/\\d\\d/\\d\\d\\d\\d<", x))==0){
-            print(paste(team,' did not play any games in ', year, sep=""))}
-          else{
-            game_results=get_game_results(x)
-            game_dates=get_game_date(x)
-            game_opploc=get_game_opponent(x)
-            game_id=as.numeric(get_game_id(x))
-            # Put everything in a data frame
-            team_schedule <- data.frame(year=year, date=game_dates, game_id=game_id, team=team, opploc=game_opploc, result=game_results, stringsAsFactors=FALSE)%>%
-              mutate(
-                # location = home (H) / away (A) / neutral (N)
-                loc = case_when(
-                  grepl("^@", opploc) ~ "A",
-                  grepl(" @ ", opploc, fixed=TRUE) ~ "N",
-                  TRUE ~ "H"),
-                # opponent
-                opp = case_when(
-                  loc=="A" ~ gsub("@","",opploc),
-                  loc=="H" ~ opploc,
-                  loc=="N" ~ substring(opploc, 1, regexpr("@", opploc)-2)))%>%
-              # select important all but opploc
-              select(year, date, game_id, team, opp, loc, result)
-            if (i==1){
-              schedule <- team_schedule} 
-            else{
-              schedule <- rbind(schedule, team_schedule)}
-          }
-        }
-      }
-    }
-    return(schedule)
-  }
+  
+  return(teams_clean)  
 }
 
 
